a22ddc50d917d7119fc00aa7158607d21495919b
DeepSpaceHourglass <hourglass@bot.local>
2026-08-16 02:32:06 +0000

    fix: disable R8 on debug to fix startup crash - keep icons-extended removed for small APK

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -20,12 +20,8 @@ android {
 
     buildTypes {
         debug {
-            isMinifyEnabled = true
-            isShrinkResources = true
-            proguardFiles(
-                getDefaultProguardFile("proguard-android-optimize.txt"),
-                "proguard-rules.pro"
-            )
+            isMinifyEnabled = false
+            isShrinkResources = false
         }
         release {
             isMinifyEnabled = true
